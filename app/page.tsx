f4d56153e7f492c5c1e0e13cf047f200fae7a56b
isPxyhl <166020564+isPxyhl@users.noreply.github.com>
2025-01-24 17:48:49 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const isLoggedIn = !!userDataCookie
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2" includeFontPadding=false>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-4xl font-bold mb-8 text-red-500">Add Recruiter to your server!</h1>
       <p className="text-xl mb-8 text-red-400">The most suitable bot for your Squid Game Infinity Roleplay faction's Discord server!</p>
       {isLoggedIn ? (
