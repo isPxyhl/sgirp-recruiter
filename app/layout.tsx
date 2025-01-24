28d18317ff20db800b3dff5a27281aa8c631549b
isPxyhl <166020564+isPxyhl@users.noreply.github.com>
2025-01-24 19:51:01 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/x-icon" href="https://media.discordapp.net/attachments/1327708896822825092/1332316200633634910/favicon.ico.png?ex=6794cfa7&is=67937e27&hm=6dd6f743b82536b408505eebb695fd41fdfd299b2bc6de17f1d7959078baf7ee&">
+      </head>
       <body className={`${inter.className} bg-black text-red-500`}>
         <NavBar />
         <main className="min-h-screen">{children}</main>
