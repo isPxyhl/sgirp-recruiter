7ed776e8db32f73bf6fb7ab07d4ef47351110cc7
isPxyhl <166020564+isPxyhl@users.noreply.github.com>
2025-01-24 20:40:02 +0800

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,13 +11,13 @@ export default function NavBar() {
     <nav className="bg-black border-b border-white/10 p-4">
       <div className="container mx-auto flex justify-between items-center">
         
-        <Link href="/add" className="nav-link font-bold">
+        <Link href="/add" className="nav-link font-bold flex items-center space-x-2">
           <Image
             src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Untitled1_20250122035540-D0L3IWndYxbzVVHCAI8KMsSTSKR73b.png"
             alt="Logo"
             width={100}
             height={40}
-            className="h-80 w-auto rounded-full mr-4 ring-2 ring-white/20"
+            className="h-8 w-auto rounded-full mr-4 ring-2 ring-white/20"
           />
         
           Recruiter
