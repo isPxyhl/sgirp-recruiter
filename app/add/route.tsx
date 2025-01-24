b574250a50f8fec2d938d22419e6da4fe9005a85
isPxyhl <166020564+isPxyhl@users.noreply.github.com>
2025-01-24 18:58:27 +0800

    Update route.tsx

--- app/add/route.tsx
+++ app/add/route.tsx
@@ -1,1 +1,20 @@
-https://discord.com/oauth2/authorize?client_id=1326131249416241226&permissions=1690467385699574&integration_type=0&scope=bot
+import { NextResponse } from "next/server"
+
+const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID
+const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET
+const REDIRECT_URI = `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/discord/callback`
+
+export async function GET() {
+  const params = new URLSearchParams({
+    client_id: DISCORD_CLIENT_ID!,
+    redirect_uri: REDIRECT_URI,
+    permissions: 169046738569957,
+    integration_type: 0,
+    scope: "bot"
+  })
+
+  return NextResponse.redirect(`https://discord.com/api/oauth2/authorize?${params.toString()}`)
+}
+
+
+
