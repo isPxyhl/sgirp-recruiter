7ea26aa3ba5f9b35e020e965ccbc174c844ca2bd
isPxyhl <166020564+isPxyhl@users.noreply.github.com>
2025-01-24 18:13:08 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
+import NavBar from "@/components/NavBar"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-black text-red-500`}>
+        <NavBar />
         <main className="min-h-screen">{children}</main>
       </body>
     </html>
