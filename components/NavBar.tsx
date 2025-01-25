274d9d890f49a6d22bbceab86c837e9ee11ff9ea
isPxyhl <166020564+isPxyhl@users.noreply.github.com>
2025-01-25 10:50:02 +0800

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import { cookies } from "next/headers"
 import Image from "next/image"
-
+declare var avatarUrl: string 
 export default function NavBar() {
   const cookieStore = cookies()
   const userDataCookie = cookieStore.get("discord_user")
