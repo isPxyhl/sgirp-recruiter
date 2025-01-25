55f217e10638801c73f4a7c74dcc7a8694eac8f1
isPxyhl <166020564+isPxyhl@users.noreply.github.com>
2025-01-26 03:09:46 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata = {
   title: "Recruiter Bot",
   description: "Simple way to manage your SGIRP faction's discord server.",
+  image: "https://media.discordapp.net/attachments/1327708896822825092/1332316200633634910/favicon.ico.png?ex=67962127&is=6794cfa7&hm=be80b95b2466bea1f0aee811986c69bbf406597e677ba1fdce699613d2b01292&",
   icons : {
     icon: [
       {
