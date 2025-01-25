41a6774540e63366b09c33795e00438336e33529
isPxyhl <166020564+isPxyhl@users.noreply.github.com>
2025-01-25 11:58:46 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <p className="text-xl mb-8 text-white/80">Elegant, efficient.</p>
       <Link
         href="/api/auth/discord"
-        className="bg-blue text-white hover:bg-blue/90 transition-all duration-300 font-bold py-3 px-6 rounded-[15px_15px]"
+        className="bg-[#6064f4] text-white hover:bg-[#6064f4]/90 transition-all duration-300 font-bold py-3 px-6 rounded-[15px_15px]"
       >
         Sign in with Discord
       </Link>
